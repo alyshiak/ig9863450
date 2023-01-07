fc2e7288b957e4b83f7db2ae7b03ba9ff996fcee
zafarfast <zafar_fast@hotmail.com>
2023-01-07 13:40:09 +1100

    Adding files after making repo pricate

--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,7 @@ fetch('https://api.openai.com/v1/images/generations', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer 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'
+        'Authorization': 'Bearer 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'
     },
     body: JSON.stringify({
         'prompt': userTextInput,
@@ -20,8 +20,15 @@ fetch('https://api.openai.com/v1/images/generations', {
     })
 }).then((response)=>{ return response.json()}).then((data)=>{ 
     console.log(data.data[0].url)
-    // document.getElementById('ai-generate-image').setAttribute('src', "")
-    document.getElementById('ai-generate-image').setAttribute('src', data.data[0].url)
+    document.getElementById('ai-generate-image1').setAttribute('src', data.data[0].url)
+    document.getElementById('ai-generate-image2').setAttribute('src', data.data[0].url)
+    document.getElementById('ai-generate-image3').setAttribute('src', data.data[0].url)
+    document.getElementById('ai-generate-image4').setAttribute('src', data.data[0].url)
+    document.getElementById('ai-generate-image5').setAttribute('src', data.data[0].url)
+    document.getElementById('ai-generate-image6').setAttribute('src', data.data[0].url)
+    document.getElementById('ai-generate-image1').setAttribute('src', data.data[0].url)
+    document.getElementById('ai-generate-image8').setAttribute('src', data.data[0].url)
+    document.getElementById('ai-generate-image9').setAttribute('src', data.data[0].url)
 
 }).catch((error)=>{console.log(error)})
 }
